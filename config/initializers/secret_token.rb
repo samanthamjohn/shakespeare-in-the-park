3e4855193f02ba2d8c936a74ef5adc19780b0ae5
Samantha John <samantha.m.john@gmail.com>
2012-07-23 09:36:43 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShakespeareInThePark::Application.config.secret_token = '1550a7de0bdc4f62e63e0788c31012fb4ee274579dac37e125d552a0dee6bd2f70df498c00ed3877b62c9f615e791428e1643faa618f727a5d79deaf1c030436'
