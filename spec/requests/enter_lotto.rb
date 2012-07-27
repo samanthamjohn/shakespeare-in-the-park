ab3110da5ba44919512e2b221778bebe1899f3ad
Samantha John <samantha.m.john@gmail.com>
2012-07-27 11:38:51 -0400

    fix css selector bug

--- spec/requests/enter_lotto.rb
+++ spec/requests/enter_lotto.rb
@@ -8,13 +8,23 @@ describe "I am entering the lotto", js: true do
       fill_in "LoginForm_Username", with: user.username
       fill_in "LoginForm_Password", with: user.password
       click_on "Sign in"
-      fill_in "VtixFormStep1_FullName", with: user.full_name
-      fill_in "VtixFormStep1_StreetAddress", with: user.address
-      select "General", from: "VtixFormStep1_SeatingPref"
-      click_on "Continue"
-      check "VtixFormStep2_Agree"
-      click_on "Submit entry"
-      page.should have_content "You have successfully entered today's Virtual Ticket Drawing for General seating!"
+
+      if page.has_css?("#VtixFormStep1_FullName")
+        fill_in "VtixFormStep1_FullName", with: user.full_name
+        fill_in "VtixFormStep1_StreetAddress", with: user.address
+        select "General", from: "VtixFormStep1_SeatingPref"
+        click_on "Continue"
+        check "VtixFormStep2_Agree"
+        click_on "Submit entry"
+        page.should have_content "You have successfully entered today's Virtual Ticket Drawing for General seating!"
+        puts "#{user.full_name} has successfully entered"
+      elsif page.has_content? "Your entry has been accepted."
+        puts "#{user.full_name} has already entered"
+      elsif page.has_content? "Unable to sign in with that username and password"
+        puts "#{user.full_name} has the wrong username or password"
+      else
+        puts "something went wrong with #{user.full_name}"
+      end
       click_on "Sign out"
     end
   end
